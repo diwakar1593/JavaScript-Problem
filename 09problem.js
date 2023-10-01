1b8a4776be869d26f6e782ed9fd9c570c8b85213
Diwakar Kumar <diwakar200kumar@gmail.com>
2023-10-01 14:11:01 +0530

    Update 09problem.js

--- 09problem.js
+++ 09problem.js
@@ -1,13 +1,34 @@
-// Qr code generator
+/* 
 
-const prompt = require("prompt-sync")({ sigint: true });
-const qr = require("qr-image");
-const fs = require("fs");
+Problem 5: FizzBuzz
 
+Write a function that prints numbers from 1 to n, but for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz." For numbers which are multiples of both 3 and 5, print "FizzBuzz."
 
-let urlInput = prompt("Enter your URL  ");
-console.log(`Your URL is: ${urlInput}`)
+*/
 
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+      } else if (i % 3 === 0) {
+        console.log('Fizz');
+      } else if (i % 5 === 0) {
+        console.log('Buzz');
+      } else {
+        console.log(i);
+      }
+    }
+  }
+  
+  fizzBuzz(15);
+  
+// remove duplicates from an array
 
-var qr_svg = qr.image(urlInput);
-qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+let arr = ["apple", "mango", "apple",
+          "orange", "mango", "mango"];
+  
+function removeDuplicates(arr) {
+    return arr.filter((item,
+        index) => arr.indexOf(item) === index);
+}
+console.log(removeDuplicates(arr));
