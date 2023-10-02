c96cef4c52c72c86e28bd9919e6f42752a7d28b2
A18yan <ma18yan@gmail.com>
2023-10-02 22:41:29 +0545

    06-problem-insertion-sort

---
+++ 06Problem.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let temp = arr[i];
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
+
+const arr = [5, 3, 6, 2, 10, 1, 9];
+
+console.log(insertionSort(arr));
